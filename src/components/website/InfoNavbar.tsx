0a1e75447c285947b5eb1b65064f916b56f412d9
brunodias77 <brunohdias95@gmail.com>
2024-01-10 15:20:55 -0300

    feat: initNavbar

---
+++ src/components/website/InfoNavbar.tsx
@@ -0,0 +1,10 @@
+export default function InfoNavBar() {
+    return (
+        <div className="flex items-center justify-between text-white  bg-[#4e4e4e] w-screen">
+            <span>Grande oportunidade no liquida total</span>
+            <span>Produtos com ate <span>50% OFF</span></span>
+            <button className="bg-[#07AC4F] py-1 px-2 uppercase">Aproveite</button>
+        </div>
+
+    )
+}
---
+++ src/components/website/Navbar.tsx
@@ -0,0 +1,9 @@
+import InfoNavBar from "./InfoNavbar";
+
+export default function NavBar() {
+    return (
+        <div className="w-screen">
+            <InfoNavBar />
+        </div>
+    )
+}
